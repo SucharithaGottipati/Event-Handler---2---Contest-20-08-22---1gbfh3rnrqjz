5165d84fa5a756e5a225dae4431b0f6d26438e51
Sucharitha16 <108065762+SucharithaGottipati@users.noreply.github.com>
2022-08-20 22:52:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,42 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+import React, { useState, useRef } from 'react';
+/**
+ * @task :add validation to email, if email is not valid, if not valid email, dont allow to submit
+ * @error_message :  "Email is invalid"  if email is wrong. (must be same message) 
+ * 
+ * 
+ */
 
-  const handleClick = (event) =>{
-    // use console.log
+function App() {
 
-  }
+ /**
+  * code here
+  */
 
-  // do not remove the two buttons or change their id
-  return (
-    <div id="main">
-      <button id="button-a">Button A</button>
-      <button id="button-b">Button B</button>
+  return(
+    <div className="App">
+      <h1>How About Them Apples</h1>
+      <form>
+        <fieldset>
+          <label>
+            <p>First Name</p>
+            <input id='fname' name="name"  ref={fnameRef}/>
+            <br></br>
+            <p>Email</p>
+            <input id='lname' name="name"   ref={emailRef}/>
+            {error && <h2 style={{color: 'red'}}>{error}</h2>}
+          </label>
+        </fieldset>
+
+        <button id='submit' type="submit">Submit</button>
+      </form>
+      {
+        data.fname != undefined && (
+          <div>
+          <h1>{data.fname}</h1>
+          <h2>{data.lname}</h2>
+          </div>
+        )
+      }
     </div>
   )
 }
